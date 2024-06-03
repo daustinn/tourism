a7e90df4973fe16d4dfe07fd75399f2423423a64
Daustinn <daustinn.dev@gmail.com>
2024-06-03 18:42:46 -0500

    refactor: Update Places component to filter places based on search category

--- src/ui/components/body/places/index.jsx
+++ src/ui/components/body/places/index.jsx
@@ -2,12 +2,24 @@
 import React from 'react'
 import { Place } from './place'
 import mockup from '@/mockups/places.json'
-export async function Places() {
+export async function Places({ searchParams }) {
   // const placesRef = db.collection('places')
   // const snapshot = await placesRef.get()
+
+  const category = searchParams.category ?? 'all'
+
+  const places = mockup.filter((place) => {
+    if (category === 'all') return true
+    return place.category === category
+  })
   return (
     <div className="grid grid-cols-3 pb-10 gap-x-10 gap-y-5 max-md:grid-cols-2 max-sm:gap-x-5 max-sm:gap-y-2">
-      {mockup.map((place, index) => (
+      {places.length === 0 && (
+        <div className="col-span-3 text-center py-36 text-gray-500">
+          No places found
+        </div>
+      )}
+      {places.map((place, index) => (
         <Place
           category={place.category}
           description={place.description}

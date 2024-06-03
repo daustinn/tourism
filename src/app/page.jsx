813264adecfbb10f5f5a72305d05054d112f9cc5
Daustinn <daustinn.dev@gmail.com>
2024-06-03 18:42:36 -0500

    chore: Update Home component to include searchParams prop

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,18 +4,18 @@ import { LineOnduled } from '@/ui/components/line-unduled'
 import { SetViews } from '@/ui/components/set'
 import { Suspense } from 'react'
 
-export default function Home() {
+export default function Home({ searchParams }) {
   return (
     <main className="flex-grow flex flex-col">
       <Suspense fallback={<></>}>
         <SetViews />
       </Suspense>
-      <div className="max-w-5xl mx-auto px-4">
+      <div className="max-w-5xl w-full mx-auto px-4">
         <Hero />
       </div>
       <LineOnduled />
-      <div className="max-w-5xl flex-grow mx-auto px-4">
-        <Body />
+      <div className="max-w-5xl w-full flex-grow mx-auto px-4">
+        <Body searchParams={searchParams} />
       </div>
     </main>
   )

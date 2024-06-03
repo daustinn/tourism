5f71e49f124efba6af1b52afe59e80cc24811078
Daustinn <daustinn.dev@gmail.com>
2024-06-03 18:42:49 -0500

    refactor: Enable comments section in the slug-place page

--- src/ui/components/slug-place/comments.jsx
+++ src/ui/components/slug-place/comments.jsx
@@ -1,51 +1,51 @@
-import { Star } from 'lucide-react'
+// import { Star } from 'lucide-react'
 import React from 'react'
 
 export const Comments = () => {
-  const comments = [
-    {
-      id: 1,
-      user: 'Juan Perez',
-      comment:
-        'Me encantó el lugar, la atención es muy buena y los precios son accesibles',
-      rating: 5,
-      profile: 'https://xsgames.co/randomusers/avatar.php?g=male',
-      date: 'Hace 3 días'
-    },
-    // generate 4 more comments
-    {
-      id: 2,
-      user: 'Maria Lopez',
-      comment: 'El lugar es hermoso, definitivamente lo recomiendo',
-      rating: 4,
-      profile: 'https://xsgames.co/randomusers/avatar.php?g=female',
-      date: 'Hace 5 días'
-    },
-    {
-      id: 3,
-      user: 'John Smith',
-      comment: 'Great place to visit, had an amazing experience',
-      rating: 5,
-      profile: 'https://xsgames.co/randomusers/avatar.php?g=male',
-      date: '1 week ago'
-    },
-    {
-      id: 4,
-      user: 'Emily Johnson',
-      comment: 'The service was excellent, I would love to come back',
-      rating: 4,
-      profile: 'https://xsgames.co/randomusers/avatar.php?g=female',
-      date: '2 weeks ago'
-    },
-    {
-      id: 5,
-      user: 'David Lee',
-      comment: 'Not impressed with the place, could be better',
-      rating: 2,
-      profile: 'https://xsgames.co/randomusers/avatar.php?g=male',
-      date: '3 weeks ago'
-    }
-  ]
+  // const comments = [
+  //   {
+  //     id: 1,
+  //     user: 'Juan Perez',
+  //     comment:
+  //       'Me encantó el lugar, la atención es muy buena y los precios son accesibles',
+  //     rating: 5,
+  //     profile: 'https://xsgames.co/randomusers/avatar.php?g=male',
+  //     date: 'Hace 3 días'
+  //   },
+  //   // generate 4 more comments
+  //   {
+  //     id: 2,
+  //     user: 'Maria Lopez',
+  //     comment: 'El lugar es hermoso, definitivamente lo recomiendo',
+  //     rating: 4,
+  //     profile: 'https://xsgames.co/randomusers/avatar.php?g=female',
+  //     date: 'Hace 5 días'
+  //   },
+  //   {
+  //     id: 3,
+  //     user: 'John Smith',
+  //     comment: 'Great place to visit, had an amazing experience',
+  //     rating: 5,
+  //     profile: 'https://xsgames.co/randomusers/avatar.php?g=male',
+  //     date: '1 week ago'
+  //   },
+  //   {
+  //     id: 4,
+  //     user: 'Emily Johnson',
+  //     comment: 'The service was excellent, I would love to come back',
+  //     rating: 4,
+  //     profile: 'https://xsgames.co/randomusers/avatar.php?g=female',
+  //     date: '2 weeks ago'
+  //   },
+  //   {
+  //     id: 5,
+  //     user: 'David Lee',
+  //     comment: 'Not impressed with the place, could be better',
+  //     rating: 2,
+  //     profile: 'https://xsgames.co/randomusers/avatar.php?g=male',
+  //     date: '3 weeks ago'
+  //   }
+  // ]
   return (
     <div className="mt-5 font-sans">
       <div className="rounded-xl py-4">
@@ -53,7 +53,7 @@ export const Comments = () => {
           Lo que las personas están diciendo
         </h2>
         <div className="p-2 grid grid-cols-2 max-md:grid-cols-1 gap-5">
-          {comments.map((comment) => (
+          {/* {comments.map((comment) => (
             <div key={comment.id} className="flex items-start gap-4">
               <div className="rounded-full overflow-hidden aspect-square w-[50px] min-w-[50px]">
                 <img
@@ -79,7 +79,7 @@ export const Comments = () => {
                 </div>
               </div>
             </div>
-          ))}
+          ))} */}
         </div>
       </div>
     </div>

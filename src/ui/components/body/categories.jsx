f5d8ce33f5a59075bfbada2c5a58a743af7ca1e4
Daustinn <daustinn.dev@gmail.com>
2024-06-05 12:38:40 -0500

    refactor: Update Categories component to use imported icons for each category

--- src/ui/components/body/categories.jsx
+++ src/ui/components/body/categories.jsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { BrickWall } from 'lucide-react'
 import {
   Church,
   RollerCoaster,
@@ -13,53 +14,58 @@ import {
 import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import React from 'react'
 
+export const categories = [
+  {
+    key: 'all',
+    traduction: 'Todos',
+    icon: GalleryHorizontal
+  },
+  {
+    key: 'churches',
+    traduction: 'Iglesias',
+    icon: Church
+  },
+  {
+    key: 'museums',
+    traduction: 'Museos',
+    icon: RollerCoaster
+  },
+  {
+    key: 'ruins',
+    traduction: 'Ruinas',
+    icon: BrickWall
+  },
+  {
+    key: 'parks',
+    traduction: 'Parques',
+    icon: Trees
+  },
+  {
+    key: 'restaurants',
+    traduction: 'Restaurantes',
+    icon: UtensilsCrossed
+  },
+  {
+    key: 'hotels',
+    traduction: 'Hoteles',
+    icon: Hotel
+  },
+  {
+    key: 'places',
+    traduction: 'Lugares',
+    icon: MapPin
+  },
+  {
+    key: 'events',
+    traduction: 'Eventos',
+    icon: BookImage
+  }
+]
+
 export function Categories() {
   const pathname = usePathname()
   const router = useRouter()
   const searchParams = useSearchParams()
-
-  const categories = [
-    {
-      key: 'all',
-      traduction: 'Todos',
-      icon: <GalleryHorizontal size={20} />
-    },
-    {
-      key: 'churches',
-      traduction: 'Iglesias',
-      icon: <Church size={20} />
-    },
-    {
-      key: 'museums',
-      traduction: 'Museos',
-      icon: <RollerCoaster size={20} />
-    },
-    {
-      key: 'parks',
-      traduction: 'Parques',
-      icon: <Trees size={20} />
-    },
-    {
-      key: 'restaurants',
-      traduction: 'Restaurantes',
-      icon: <UtensilsCrossed size={20} />
-    },
-    {
-      key: 'hotels',
-      traduction: 'Hoteles',
-      icon: <Hotel size={20} />
-    },
-    {
-      key: 'places',
-      traduction: 'Lugares',
-      icon: <MapPin size={20} />
-    },
-    {
-      key: 'events',
-      traduction: 'Eventos',
-      icon: <BookImage size={20} />
-    }
-  ]
 
   const handleClick = (key) => {
     const params = new URLSearchParams(searchParams)
@@ -80,7 +86,7 @@ export function Categories() {
           key={category.key}
           className="flex font-medium group aria-selected:text-black text-neutral-600 items-center text-xs flex-col"
         >
-          {category.icon}
+          <category.icon size={25} />
           <span className="px-3 py-2">{category.traduction}</span>
           <span className="w-full transition-all h-[2px] group-aria-selected:opacity-100 group-aria-selected:bg-black group-hover:opacity-100 opacity-0 bg-neutral-400 block" />
         </button>

0eff169a2a2230e42b30dfab22591df8e816ea30
Daustinn <daustinn.dev@gmail.com>
2024-06-05 14:01:18 -0500

    refactor: Update PlacePage component to use async/await for fetching place data

--- src/app/places/[slug]/page.jsx
+++ src/app/places/[slug]/page.jsx
@@ -1,28 +1,30 @@
 import React from 'react'
-import mockup from '@/mockups/places.json'
-import { redirect } from 'next/navigation'
 import { PlaceSlug } from '@/ui/components/slug-place'
+import { getPlace } from '@/services/places'
 
-export function generateMetadata({ params }) {
+export async function generateMetadata({ params }) {
   const parserSlug = decodeURIComponent(params.slug)
-
-  const place = mockup.find(
-    (place) => place.title.toLowerCase() === parserSlug.toLowerCase()
-  )
-
-  return {
-    title: place.title + ' | Ayacucho',
-    description: 'Place description'
+  try {
+    const place = await getPlace(parserSlug)
+    return {
+      title: place.title + `' | Ayacucho'`,
+      description: place.description,
+      openGraph: {
+        images: [place.thumbnail, ...place.images]
+      }
+    }
+  } catch (error) {
+    console.error(error)
   }
 }
-export default function PlacePage({ params: { slug } }) {
+export default async function PlacePage({ params: { slug } }) {
   const parserSlug = decodeURIComponent(slug)
-  const place = mockup.find(
-    (place) => place.title.toLowerCase() === parserSlug.toLowerCase()
-  )
-  if (!place) {
-    return redirect('/')
+  try {
+    // delay
+    // await new Promise((resolve) => setTimeout(resolve, 5000))
+    const place = await getPlace(parserSlug)
+    return <PlaceSlug place={place} />
+  } catch (error) {
+    console.error(error)
   }
-
-  return <PlaceSlug place={place} />
 }

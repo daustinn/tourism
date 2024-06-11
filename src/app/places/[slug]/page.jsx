b60775bd5feb4c2a4e837099198dcccb9d700764
Daustinn <daustinn.dev@gmail.com>
2024-06-11 08:29:19 -0500

    refactor: Add lazy loading for images in PlacePage component

--- src/app/places/[slug]/page.jsx
+++ src/app/places/[slug]/page.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { PlaceSlug } from '@/ui/components/slug-place'
 import { getPlace } from '@/services/places'
+import { notFound } from 'next/navigation'
 
 export async function generateMetadata({ params }) {
   const parserSlug = decodeURIComponent(params.slug)
@@ -8,7 +9,7 @@ export async function generateMetadata({ params }) {
     const place = await getPlace(parserSlug)
     return {
       title: place.title + `' | Ayacucho'`,
-      description: place.description,
+      description: place?.description,
       openGraph: {
         images: [place.thumbnail, ...place.images]
       }
@@ -21,7 +22,8 @@ export default async function PlacePage({ params: { slug } }) {
   const parserSlug = decodeURIComponent(slug)
   try {
     const place = await getPlace(parserSlug)
-    return <PlaceSlug place={place} />
+    if (!place) notFound()
+    return <PlaceSlug place={{ id: parserSlug, ...place }} />
   } catch (error) {
     console.error(error)
   }

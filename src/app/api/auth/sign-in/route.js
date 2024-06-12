70f6e3303d5f4460e0e85f808664349a66416876
Daustinn <daustinn.dev@gmail.com>
2024-06-12 13:14:02 -0500

    refactor: Update sign-in API route to validate form data and handle errors

--- src/app/api/auth/sign-in/route.js
+++ src/app/api/auth/sign-in/route.js
@@ -1,7 +1,32 @@
 import { NextResponse } from 'next/server'
+import { Auth } from 'services/auth'
+import { z } from 'zod'
 
 // sign in with credentials
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function POST(req) {
-  return NextResponse.json({ message: 'SIGN AUTH WITH CREDENTIAl' })
+  try {
+    const form = validate.safeParse(await req.json())
+    if (!form.success) throw new Error('invalid_form')
+    const { email, password } = form.data
+
+    const sucess = await Auth.login({
+      email,
+      password
+    })
+
+    return NextResponse.json(sucess)
+  } catch (error) {
+    return NextResponse.json(
+      { error: error.message },
+      {
+        status: 400
+      }
+    )
+  }
 }
+
+const validate = z.object({
+  email: z.string(),
+  password: z.string()
+})

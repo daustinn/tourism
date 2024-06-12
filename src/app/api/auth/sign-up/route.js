5fb8aaeec53f983cc1f30f4485d6202cef397c1a
Daustinn <daustinn.dev@gmail.com>
2024-06-12 13:14:05 -0500

    refactor: Update sign-up API route to validate form data and handle errors

--- src/app/api/auth/sign-up/route.js
+++ src/app/api/auth/sign-up/route.js
@@ -1,7 +1,44 @@
 import { NextResponse } from 'next/server'
+import { User } from 'services/users'
+import { hashPassword } from 'utils/bcrypt'
+import { z } from 'zod'
 
 // sign up with credentials
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function POST(req) {
-  return NextResponse.json({ message: 'CREATE USER WITH CREDENTIAl' })
+  try {
+    const form = validate.safeParse(await req.json())
+    if (!form.success) throw new Error(form.error.message)
+    const { password, email, name, profile } = form.data
+
+    const readUser = await User.get({ email })
+    if (readUser) throw new Error('user_already_exists')
+
+    const hashedPassword = await hashPassword(password)
+
+    const userData = {
+      email,
+      password: hashedPassword,
+      name,
+      profile
+    }
+
+    const newUser = await User.create(userData)
+
+    return NextResponse.json({ user: newUser })
+  } catch (error) {
+    return NextResponse.json(
+      { error: error.message },
+      {
+        status: 400
+      }
+    )
+  }
 }
+
+const validate = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+  name: z.string().min(3),
+  profile: z.string().url()
+})

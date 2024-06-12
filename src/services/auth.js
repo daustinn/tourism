16e1a7077caa55c7951188930b8609691248e591
Daustinn <daustinn.dev@gmail.com>
2024-06-12 13:14:15 -0500

    refactor: Add login functionality to Auth service

---
+++ src/services/auth.js
@@ -0,0 +1,20 @@
+import { verifyPassword } from 'utils/bcrypt'
+import { User } from './users'
+import { sign } from 'libs/jwt'
+
+class Auth {
+  static async login({ email, password }) {
+    const finded = await User.get({ email })
+    if (!finded) throw new Error('user_not_found')
+    const verified = await verifyPassword(password, finded.password)
+    if (!verified) throw new Error('invalid_password')
+
+    const token = sign({ email: finded.email, id: finded.id })
+    return {
+      token,
+      user: finded
+    }
+  }
+}
+
+export { Auth }

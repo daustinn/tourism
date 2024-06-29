6d7c6f637b78d43f9d127664f85b2dc1650b40c6
Daustinn <daustinn.dev@gmail.com>
2024-06-29 15:13:58 -0500

    refactor: Update sign-up API route to include JWT token in response

--- src/app/api/auth/sign-up/route.js
+++ src/app/api/auth/sign-up/route.js
@@ -1,3 +1,5 @@
+import { sign } from 'libs/jwt'
+import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 import { User } from 'services/users'
 import { hashPassword } from 'utils/bcrypt'
@@ -16,16 +18,31 @@ export async function POST(req) {
 
     const hashedPassword = await hashPassword(password)
 
+    const number = Math.floor(Math.random() * 4) + 1
+
     const userData = {
       email,
       password: hashedPassword,
       name,
-      profile
+      profile: profile ?? `/default-profiles/${number}.png`
     }
 
     const newUser = await User.create(userData)
 
-    return NextResponse.json({ user: newUser })
+    const token = sign({
+      email: newUser.email,
+      id: newUser.id,
+      profile: newUser.profile
+    })
+
+    const store = cookies(req)
+    store.set('auth_token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict'
+    })
+
+    return NextResponse.json({ user: newUser, token })
   } catch (error) {
     return NextResponse.json(
       { error: error.message },
@@ -40,5 +57,5 @@ const validate = z.object({
   email: z.string().email(),
   password: z.string().min(6),
   name: z.string().min(3),
-  profile: z.string().url()
+  profile: z.string().optional().nullable()
 })

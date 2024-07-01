961256b8406c947ddb7c4af4a6eed6d8a6168bb4
Daustinn <daustinn.dev@gmail.com>
2024-07-01 00:10:55 -0500

    refactor: Add GET route for retrieving place details by slug

---
+++ src/app/api/places/[slug]/route.js
@@ -0,0 +1,23 @@
+import { NextResponse } from 'next/server'
+import { getPlace } from 'services/places'
+import { auth } from 'utils/middleware-apis'
+
+export async function GET(req, { params }) {
+  try {
+    // verify the request is authorized
+    auth(req)
+
+    const slug = params.slug
+    const place = await getPlace(slug)
+    return NextResponse.json(place)
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: error.message
+      },
+      {
+        status: 500
+      }
+    )
+  }
+}

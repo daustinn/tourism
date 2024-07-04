13dcd3a139b863ef9a97140bbb2831e4c63b9324
Daustinn <daustinn.music@gmail.com>
2024-07-04 11:04:54 -0500

    refactor: Update header test to use correct text for header rendering

--- src/__test__/ui/component/header/index.test.jsx
+++ src/__test__/ui/component/header/index.test.jsx
@@ -1,9 +1,9 @@
-import { Header } from '@/ui/components/header'
+import { Header } from 'ui/components/header'
 import { render, screen } from '@testing-library/react'
 
 describe('Header', () => {
   it('should render the header', () => {
     render(<Header />)
-    expect(screen.getByText('Inicia Sesión')).toBeInTheDocument()
+    expect(screen.getByText('Descubre Ayacucho')).toBeInTheDocument()
   })
 })
